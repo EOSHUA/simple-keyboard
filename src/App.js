f2e7f4ce3d26b058d574e4dae6f03b4d51b1ca5e
j.r. <h6141362@gmail.com>
2023-11-09 18:22:08 +0200

    new project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const lettersEnglish = "abcdefghijklmnopqrstuvwxyz".split("");
+  const lettersHebrew = "אבגדהוזחטיכךלמנסעפףצץקרשת".split("");
+  const numbers = "1234567890".split("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {lettersEnglish.map((lettersEnglish, index) => (
+        <lettersEnglish key={index} lettersEnglish={lettersEnglish} />
+      ))}
+
+
+      <p className="App-intro">
+        {lettersEnglish}
+      </p>
+      <p className="App-intro">
+        {lettersHebrew}
+      </p>
+      <p className="App-intro">
+        {numbers}
+      </p>
+
     </div>
   );
 }
